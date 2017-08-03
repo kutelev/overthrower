716ec7357cfbfac358cf41d282b74d4670954bb2
Alexander Kutelev <alexander@kutelev.ru>
2017-08-03 12:00:27 +0700

    Added test for laziness.

--- laziness/main.c
+++ laziness/main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -6,6 +7,7 @@ void activateOverthrower() __attribute__ ((weak));
 int main(int argc, char** argv)
 {
     int i;
+    char tmp[1024];
 
     // Meaningless lines below activate overthrower if preloaded
     if (activateOverthrower != NULL)
@@ -14,8 +16,11 @@ int main(int argc, char** argv)
     for (i = 0; i < 100500; ++i) {
         void* data = malloc(100500);
         memset(data, 0, 100500);
+        sprintf(tmp, "data = %p\n", data);
         free(data);
     }
+
+    printf("%s", tmp);
 
     return 0;
 }
---
+++ tests/test.py
@@ -0,0 +1,29 @@
+import sys
+import subprocess
+import platform
+import os
+
+environment_variables = dict()
+
+if platform.system() == 'Linux':
+    environment_variables['LD_LIBRARY_PATH'] = './overthrower'
+    environment_variables['LD_PRELOAD'] = 'liboverthrower.so'
+elif platform.system() == 'Darwin':
+    environment_variables['DYLD_FORCE_FLAT_NAMESPACE'] = '1'
+    environment_variables['DYLD_INSERT_LIBRARIES'] = './overthrower/overthrower.framework/overthrower'
+else:
+    sys.exit(1)
+
+laziness = ['./laziness/laziness']
+
+subprocess.check_call(laziness)
+
+for key in environment_variables:
+    os.putenv(key, environment_variables[key])
+
+try:
+    subprocess.check_call(laziness)
+    sys.exit(1)
+except subprocess.CalledProcessError as e:
+    print('laziness has failed as expected. return code is {}.'.format(e.returncode))
+    pass
