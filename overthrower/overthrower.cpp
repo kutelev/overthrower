b5327aaeb3a69e40ba8b239c4ba3e4b96f8b5a5c
Alexander Kutelev <alexander@kutelev.ru>
2017-11-28 13:42:10 +0700

    __cxa_allocate_exception can occur on another depth.

--- overthrower/overthrower.cpp
+++ overthrower/overthrower.cpp
@@ -327,7 +327,7 @@ static void searchKnowledgeBase(bool& is_in_white_list, bool& is_in_ignore_list)
     is_in_ignore_list = false;
 
 #if defined(__APPLE__)
-    if (count >= 4 && strstr(symbols[3], "__cxa_allocate_exception"))
+    if (count >= 4 && (strstr(symbols[3], "__cxa_allocate_exception") || strstr(symbols[2], "__cxa_allocate_exception")))
         is_in_white_list = true;
     if (count >= 4 && (strstr(symbols[3], "__cxa_atexit") || strstr(symbols[2], "__cxa_atexit"))) {
         is_in_white_list = true;
